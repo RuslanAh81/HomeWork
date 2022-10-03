41e08ed15b1ef73b577efc05217232d72c86396a
Ruslan <arhara81@gmail.com>
2022-10-03 17:53:08 +0300

    Dobavil vtoruyu zada4u

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+//  Задача номер 1. Напишите программу, которая на вход принимает 2 числа и выдает,какое число больше.
+Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine( a + " Больше чем " + b);
+}
+else
+{
+       Console.WriteLine( b + " Больше чем " + a);
+}
+*/
+
+/*
+// Задача 2. Напишите программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел.
+
+Console.Write("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число с:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+ Console.WriteLine(max);
+
+*/
+// Задача 3. Напишите программу, которая на вход принимает число и выдает, является ли число четным.
+
+console.Write("Введите число")
